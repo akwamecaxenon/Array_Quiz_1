3577e2add5ad6e0b331042801778e186b8c05192
akwamecaxenon <kzzfell536@gmail.com>
2025-08-21 20:22:48 +0800

    My quiz answers

--- Array.cpp
+++ Array.cpp
@@ -3,19 +3,23 @@
 #include "Array.h"
 
 // Constructor: allocate array with given capacity
-Array::Array(int capacity) : capacity_(capacity), size_(0) {
+Array::Array(int capacity) : capacity_(capacity), size_(0)
+{
     data_ = new int[capacity_];
 }
 
 // Destructor: free allocated memory
-Array::~Array() {
+Array::~Array()
+{
     delete[] data_;
 }
 
 // Add value at the end if there's space
-bool Array::Add(int value) {
-    if (size_ == capacity_) {
-        return false;  // no space
+bool Array::Add(int value)
+{
+    if (size_ == capacity_)
+    {
+        return false; // no space
     }
     data_[size_] = value;
     size_++;
@@ -23,47 +27,82 @@ bool Array::Add(int value) {
 }
 
 // Insert value at specified index, shifting elements to the right
-bool Array::InsertAt(int index, int value) {
-    return false;
+bool Array::InsertAt(int index, int value)
+{
+    if (index < 0 || index > size_ || size_ == capacity_)
+    {
+        return false;
+    }
+    for (int i = size_; i > index; i--)
+    {
+        data_[i] = data_[i - 1];
+    }
+    data_[index] = value;
+    size_++;
+    return true;
 }
 
 // Delete element at index, shifting elements left
-bool Array::DeleteAt(int index) {
-    return false;
+bool Array::DeleteAt(int index)
+{
+    if (index < 0 || index >= size_)
+    {
+        return false;
+    }
+    for (int i = index; i < size_ - 1; i++)
+    {
+        data_[i] = data_[i + 1];
+    }
+    size_--;
+    return true;
 }
 
 // Update element at index
-bool Array::UpdateAt(int index, int new_value) {
-    return false;
+bool Array::UpdateAt(int index, int new_value)
+{
+    if (index < 0 || index >= size_)
+    {
+        return false;
+    }
+    data_[index] = new_value;
+    return true;
 }
 
 // Print all elements
-void Array::Print() const {
-    if (size_ == 0) {
+void Array::Print() const
+{
+    if (size_ == 0)
+    {
         std::cout << "[Empty]" << std::endl;
         return;
     }
     std::cout << "[ ";
-    for (int i = 0; i < size_; i++) {
+    for (int i = 0; i < size_; i++)
+    {
         std::cout << data_[i];
-        if (i != size_ - 1) std::cout << ", ";
+        if (i != size_ - 1)
+            std::cout << ", ";
     }
     std::cout << " ]" << std::endl;
 }
 
 // Get current size
-int Array::Size() const {
+int Array::Size() const
+{
     return size_;
 }
 
 // Get max capacity
-int Array::Capacity() const {
+int Array::Capacity() const
+{
     return capacity_;
 }
 
 // Get value at index or throw if invalid
-int Array::GetAt(int index) const {
-    if (index < 0 || index >= size_) {
+int Array::GetAt(int index) const
+{
+    if (index < 0 || index >= size_)
+    {
         throw std::out_of_range("Index out of range");
     }
     return data_[index];
